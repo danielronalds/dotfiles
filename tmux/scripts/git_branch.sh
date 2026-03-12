9c7e09bf42208efdc7d058b16869cbfdcf3f7222
Daniel Ronalds <nzdanron@gmail.com>
2026-03-13 11:03:06 +1300

    fix(tmux): add short mode for branch name display

--- tmux/scripts/git_branch.sh
+++ tmux/scripts/git_branch.sh
@@ -8,6 +8,12 @@ main() {
         return
     fi
 
+    local show_branch_short=$(tmux show-option -gqv @show_git_branch_short)
+
+    if [[ "$show_branch_short" == "1" ]]; then
+        branch="${branch:0:15}"
+    fi
+
     echo -n " $branch"
 }
 
